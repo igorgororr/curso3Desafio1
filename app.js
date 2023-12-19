880cad2acd69581730beb590d4a994424ae52f28
Igor Godoi Camargo <igor.godoi.camargo@gmail.com>
2023-12-19 08:49:29 -0300

    Só Para Apagar

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Testante se da certo o repositório
 console.log(10000);
 // git pull origin main --allow-unrelated-histories
-// Apagar git reser --hard 
+// Apagar git reset --hard 
 // git commit --amend -m "novo nome"
+// Só para apagar
