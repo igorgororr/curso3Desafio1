0b186df8b393fb9fffa9f8f5779a92435967fe13
Igor Godoi Camargo <igor.godoi.camargo@gmail.com>
2023-12-21 16:48:55 -0300

    Desafio 2

--- app.js
+++ app.js
@@ -3,3 +3,6 @@ console.log(10000);
 // git pull origin main --allow-unrelated-histories
 // Apagar git reset --hard 
 // git commit --amend -m "novo nome"
+// git revert -n <hash-do-ultimo-commit-a-manter> para reverter mais de um commit, depois de reverter lembrar de realizar um commit novo para aplicar a versão ao repositório
+// Se você apenas deseja desfazer commits, mas manter as alterações no diretório de trabalho, você pode usar git reset --soft
+// git log > logs.txt
